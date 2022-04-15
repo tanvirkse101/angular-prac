dd4222b0b2bb2bb34d0eb08059ce313fdb39e641
tanvirkse101 <tanvirkse@gmail.com>
2022-04-16 01:05:54 +0600

    services

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
     selector: 'app-signup',
@@ -6,11 +7,30 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-    test : Date = new Date();
-    focus;
-    focus1;
-    focus2;
-    constructor() { }
+    form: any = {
+        username: null,
+        email: null,
+        password: null
+    };
+    isSuccessful = false;
+    isSignUpFailed = false;
+    errorMessage = '';
+    constructor(private authService: AuthService) { }
 
-    ngOnInit() {}
+    ngOnInit() { }
+
+    onSubmit(): void {
+        const { username, email, password } = this.form;
+        this.authService.register(username, email, password).subscribe(
+            data => {
+                console.log(data);
+                this.isSuccessful = true;
+                this.isSignUpFailed = false;
+            },
+            err => {
+                this.errorMessage = err.error.message;
+                this.isSignUpFailed = true;
+            }
+        );
+    }
 }
